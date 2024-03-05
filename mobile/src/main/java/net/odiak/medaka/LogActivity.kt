9f9b025a5d11123c9e828c578326f618836745df
Kaido Iwamoto <kaido@odiak.net>
2024-03-06 06:36:46 +0900

    Add LogActivity, launchable from shortcut

---
+++ mobile/src/main/java/net/odiak/medaka/LogActivity.kt
@@ -0,0 +1,73 @@
+package net.odiak.medaka
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.LineBreak
+import net.odiak.medaka.theme.MedakaTheme
+import java.io.File
+
+class LogActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MedakaTheme {
+                Scaffold { p ->
+                    val textState = remember { mutableStateOf(getLogText()) }
+                    val scrollState = rememberScrollState()
+
+                    Column(modifier = Modifier.padding(p)) {
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .verticalScroll(scrollState)
+                                .fillMaxWidth()
+                        ) {
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                style = TextStyle(lineBreak = LineBreak.Paragraph),
+                                text = textState.value
+                            )
+                        }
+
+                        Row {
+                            Button(onClick = { textState.value = getLogText() }) {
+                                Text(text = "Reload")
+                            }
+                            Button(onClick = { clear(); textState.value = "" }) {
+                                Text(text = "Clear")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getLogText(): String {
+        val file = File(filesDir, Logger.LOG_FILE)
+        if (!file.exists()) return ""
+        return file.readText()
+    }
+
+    private fun clear() {
+        val file = File(filesDir, Logger.LOG_FILE)
+        if (file.exists()) file.delete()
+    }
+}
---
+++ mobile/src/main/java/net/odiak/medaka/Logger.kt
@@ -0,0 +1,34 @@
+package net.odiak.medaka
+
+import android.content.Context
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class Logger(private val context: Context) {
+    companion object {
+        const val LOG_FILE = "log.txt"
+    }
+
+    fun log(message: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val datetimeStr =
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+            val line = "[$datetimeStr] $message\n"
+
+            val file = File(context.filesDir, LOG_FILE)
+            if (!file.exists()) {
+                file.createNewFile()
+            }
+            val lines = file.readLines()
+            val newLines = lines.takeLast(500) + line
+            file.writeText(newLines.joinToString("\n"))
+        }
+    }
+}
+
+val Context.logger: Logger
+    get() = Logger(this)
